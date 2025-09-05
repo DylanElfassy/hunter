7d609985f4e4a53ce264fa79686db131519a5a8b
Dylou <dylanelfassy@gmail.com>
2025-09-05 09:48:20 +0300

    3d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <main>
       {/* <Hero /> */}
       
-      <HeroMap />
+      {/* <HeroMap /> */}
       <StepIntoGame />
       <Premium />
       <ReferralSection />
